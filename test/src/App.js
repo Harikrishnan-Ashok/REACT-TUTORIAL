54e99a532cbf08ddbf3e81b9de40b5352b6c45b2
Harikrishnana Ashok <harikrishnanashok2001@gmail.com>
2023-07-03 13:10:05 +0530

    day1

---
+++ test/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from "./components/Test"  // poilla
+function App() {
+  return (
+    <div className="App">
+      <Test/>
+    </div>
+  );
+}
+export default App;
---
+++ test/src/components/Test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function test()
+{
+    return <h1>babu mon oru killadi thanne</h1>
+}
+export default test
